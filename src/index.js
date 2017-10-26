8a553f25d9eaf9d034d04c2d2201dc3c1cca08b9
Samuel James Mathew <samuel_jm@geojit.com>
2017-10-26 15:31:31 +0530

    Asset Management - loading CSS files to inline style tag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
+import './style.css'
 function component() {
     var element = document.createElement('div');
+    element.classList.add('hello');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
 }
